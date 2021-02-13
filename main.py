571506ec1dda67560dc507846d7ba3729d239606
Lawrence Dovin <lawrencedovin@gmail.com>
2021-02-12 20:11:09 -0500

    Updated calculator

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 from art import logo
 
-def calculate(number1, number2, expression):
+def calculate(number1, number2, operation):
     mathematical_expression = {
         '+': number1 + number2,
         '-': number1 - number2,
         '*': number1 * number2,
         '/': number1 / number2
     }
+    answer = mathematical_expression.get(operation, "Invalid")
+    print(f"{number1} {operation} {number2} = {answer}")
 
-    print(mathematical_expression.get(expression, "Invalid"))
-
-calculate(1,2,'/')
+print(logo)
+number1 = int(input("What's the first number?: "))
+number2 = int(input("What's the second number?: "))
+operation = input("What operation would you want? + - * /: ")
+calculate(number1, number2, operation)
