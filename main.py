afc16703345c270a4ee75927cc304d4277087dd4
Lawrence Dovin <lawrencedovin@gmail.com>
2021-02-13 14:13:20 -0500

    Updated calculator

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from art import logo
 
+should_continue = True
 def calculate(number1, number2, operation):
     mathematical_expression = {
         '+': number1 + number2,
@@ -8,10 +9,18 @@ def calculate(number1, number2, operation):
         '/': number1 / number2
     }
     answer = mathematical_expression.get(operation, "Invalid")
-    print(f"{number1} {operation} {number2} = {answer}")
+    return answer
 
 print(logo)
 number1 = int(input("What's the first number?: "))
-number2 = int(input("What's the second number?: "))
-operation = input("What operation would you want? + - * /: ")
-calculate(number1, number2, operation)
+
+while should_continue:
+    number2 = int(input("What's the next number?: "))
+    operation = input("What operation would you want? + - * /: ")
+    answer = calculate(number1, number2, operation)
+    print(f"{number1} {operation} {number2} = {answer}")
+
+    if input(f"Type 'y' to continue calculating with {answer}, or type 'n' to exit.: ") == 'y':
+        number1 = answer
+    else:
+        should_continue = False
