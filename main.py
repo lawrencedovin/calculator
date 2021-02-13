7d1c2e24224180a3a17fd1fcfc2ee7173c5b8bc5
Lawrence Dovin <lawrencedovin@gmail.com>
2021-02-13 14:19:02 -0500

    Updated calculator to start new calculation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from art import logo
 
-should_continue = True
+
 def calculate(number1, number2, operation):
     mathematical_expression = {
         '+': number1 + number2,
@@ -12,15 +12,21 @@ def calculate(number1, number2, operation):
     return answer
 
 print(logo)
-number1 = int(input("What's the first number?: "))
 
-while should_continue:
-    number2 = int(input("What's the next number?: "))
-    operation = input("What operation would you want? + - * /: ")
-    answer = calculate(number1, number2, operation)
-    print(f"{number1} {operation} {number2} = {answer}")
+def calculator():
+    should_continue = True
+    number1 = int(input("What's the first number?: "))
 
-    if input(f"Type 'y' to continue calculating with {answer}, or type 'n' to exit.: ") == 'y':
-        number1 = answer
-    else:
-        should_continue = False
+    while should_continue:
+        number2 = int(input("What's the next number?: "))
+        operation = input("What operation would you want? + - * /: ")
+        answer = calculate(number1, number2, operation)
+        print(f"{number1} {operation} {number2} = {answer}")
+
+        if input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation.: ") == 'y':
+            number1 = answer
+        else:
+            should_continue = False
+            calculator()
+
+calculator()
